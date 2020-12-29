5f0ca52b0b778626e492c5e058d18656eab878eb
Dennis Mullen <dkmullen@gmail.com>
2020-12-29 15:34:26 -0500

    updates

--- main.js
+++ main.js
@@ -4,23 +4,77 @@ const submit = document.getElementById('submit')
 var app = new Vue({
     el: '#app',
     data: {
-        form: {
-            title: 'Contact Form',
-            name: '',
-            email: '',
-            message: '',
-            toast: ''
-        } 
+        title: 'Contact Form',
+        name: {
+            error: false,
+            errorMessage: '',
+            value: ''
+        },
+        email: {
+            error: false,
+            errorMessage: '',
+            value: ''
+        },
+        message: {
+            error: false,
+            errorMessage: '',
+            value: ''
+        },
+        toast: '',
+        errors: ['Required field', 'Only text is permitted', 'Invalid email address', ''],
+        errorCount: 0
     },
     methods: {
+        checkform() {
+            this.clearErrors();
+            if (!this.name.value) {
+                this.name.error = true;
+                this.name.errorMessage = this.errors[0];
+                this.errorCount++;
+            } else if (!this.validName(this.name.value)) {
+                this.name.error = true;
+                this.name.errorMessage = this.errors[1];
+                this.errorCount++;
+            }
+            if (!this.email.value) {
+                this.email.error = true;
+                this.email.errorMessage = this.errors[0];
+                this.errorCount++;
+
+            } else if (!this.validEmail(this.email.value)) {
+                this.email.error = true;
+                this.email.errorMessage = this.errors[2];
+                this.errorCount++;
+            }
+            if (this.errorCount === 0) {
+                this.submit();
+            }
+        },
+        validName(name) {
+            var re = /^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$/;
+            return re.test(name);  
+        },
+        validEmail(email) {
+            var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);  
+        },
+        clearErrors() {
+            this.errorCount = 0;
+            this.name.error = false;
+            this.name.errorMessage = '';
+            this.email.error = false;
+            this.email.errorMessage = '';
+            this.message.error = false;
+            this.message.errorMessage = '';
+        },
         submit() {
-            app.form.toast = 'Sending';
+            app.toast = 'Sending';
             submit.disabled; // Doesn't work
         
             const payload = {
-                name: app.form.name,
-                email: app.form.email,
-                message: app.form.message
+                name: app.name.value,
+                email: app.email.value,
+                message: app.message.value
             }
             post(url, payload, function (err,res)  {
                 if (err) { return error(err) }
@@ -44,17 +98,17 @@ function post(url, body, callback) {
     req.send(JSON.stringify(body));
 }
 function success() {
-    app.form.toast = 'Thanks for the message. We will get back to you shortly.'
+    app.toast = 'Thanks for the message. We will get back to you shortly.'
     setTimeout(() => {
-        app.form.toast = '';
+        app.toast = '';
         submit.disabled = false;
     }, 2000);
-    app.form.name = '';
-    app.form.email = '';
-    app.form.message = '';
+    app.name.value = '';
+    app.email.value = '';
+    app.message.value = '';
 }
 function error (err) {
-    app.form.toast = 'There was a problem submitting your message.';
+    app.toast = 'There was a problem submitting your message.';
     submit.disabled = false;
     console.log(err);
 }
